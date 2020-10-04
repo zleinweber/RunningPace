dc2facb8d2ff007e6728b54c9e6347ed35157e04
Zachary Leinweber <zleinweb@gmail.com>
2020-10-03 21:50:06 -0500

    Create a basic clonable template

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,20 @@
 import '../styles/index.scss';
 
-if (process.env.NODE_ENV === 'development') {
-  require('../index.html');
-}
+//if (process.env.NODE_ENV === 'development') {
+//  require('../index.html');
+//}
+
+const templateTopLevel = document.getElementById("docTemplates");
+templateTopLevel.hidden = true;
+
+const paceTable = document.getElementById("paceTable");
 
 let paceForm = document.getElementById("paceForm");
 paceForm.addEventListener("submit", event => {
   let numMiles = paceForm.elements['numMiles'];
   let pace = paceForm.elements['pace'];
-  console.log(numMiles.value, pace.value);
+  let paceTableView = document.getElementById("paceTemplate").cloneNode(true);
+  paceTableView.innerText = `running ${numMiles.value} miles at ${pace.value}`;
+  paceTable.appendChild(paceTableView);
   event.preventDefault();
 });
